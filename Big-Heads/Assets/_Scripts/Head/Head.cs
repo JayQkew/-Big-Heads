512a085103dbaa289e4801597fb579f9a49c862a
JayQkew <rayjebb@gmail.com>
2025-03-15 11:15:40 +0200

    small push

---
+++ Big-Heads/Assets/_Scripts/Head/Head.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Head : MonoBehaviour, IDamageable
+{
+    public int health;
+
+    [SerializeField] private float maxSize;
+    [SerializeField] private float minSize;
+    
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Damage(int dmg)
+    {
+        health -= dmg;
+        if (health <= 0)
+        {
+            Debug.Log("Player Died");
+        }
+    }
+}
