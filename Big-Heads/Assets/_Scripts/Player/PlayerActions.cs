46a5bec7320c0053c9c60fffbb3ccec4c5ef78d7
JayQkew <rayjebb@gmail.com>
2025-04-02 22:50:52 +0200

    stat changes

--- Big-Heads/Assets/_Scripts/Player/PlayerActions.cs
+++ Big-Heads/Assets/_Scripts/Player/PlayerActions.cs
@@ -10,65 +10,65 @@ public class PlayerActions : MonoBehaviour
     private StatModifiers _statModifiers;
 
     public bool headAttached = true;
-    [Space(10)] [SerializeField] private Transform head;
+
+    [Space(10)]
+    [SerializeField] private Transform head;
     [SerializeField] private Transform body;
     private Head _head;
-    [Header("Throw")] [SerializeField] private float throwForce;
 
-    [Header("Teleport")] [SerializeField] private float teleportTime;
+    [Header("Throw")]
+    [SerializeField] private float throwForce;
+
+    [Header("Teleport")]
+    [SerializeField] private float teleportTime;
     [SerializeField] private float currTeleportTime;
 
-    [Header("Shooting")] [SerializeField] private GameObject gun;
+    [Header("Shooting")]
+    [SerializeField] private GameObject gun;
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private Transform spawnPoint;
     [SerializeField] private float shootForce;
-    [Space(10)] [SerializeField] private int maxAmmo;
-    [SerializeField] private int currAmmo;
-    [Space(10)] [SerializeField] private float fireRate;
-    [SerializeField] private float currFireRate;
-    [Space(10)] [SerializeField] private int bulletBounces;
-    [SerializeField] private int bulletSize;
 
-    private void Awake()
-    {
+    [Space(10)]
+    [SerializeField] private int maxAmmo;
+    [SerializeField] private int currAmmo;
+
+    [Space(10)]
+    [SerializeField] private float fireRate;
+    [SerializeField] private float currFireRate;
+
+    private void Awake() {
         _inputHandler = GetComponent<InputHandler>();
         _statModifiers = GetComponent<StatModifiers>();
         _head = head.GetComponent<Head>();
     }
 
-    private void Start()
-    {
+    private void Start() {
         currAmmo = maxAmmo + _statModifiers.ExtraAmmo;
 
-        if (_statModifiers.AutoFire)
-        {
+        if (_statModifiers.AutoFire) {
             _inputHandler.onShoot.AddListener(Shoot);
             _inputHandler.onShootStart.RemoveListener(Shoot);
         }
-        else
-        {
+        else {
             _inputHandler.onShootStart.AddListener(Shoot);
             _inputHandler.onShoot.RemoveListener(Shoot);
         }
     }
 
-    private void Update()
-    {
+    private void Update() {
         GunDirection();
         currTeleportTime += Time.deltaTime;
         currFireRate += Time.deltaTime;
     }
 
-    public void TeleportThrow()
-    {
+    public void TeleportThrow() {
         if (headAttached) Throw();
         else Teleport();
     }
 
-    private void Teleport()
-    {
-        if (currTeleportTime * _statModifiers.TeleportReloadMult >= teleportTime)
-        {
+    private void Teleport() {
+        if (currTeleportTime * _statModifiers.TeleportReloadMult >= teleportTime) {
             body.position = head.position;
             head.position = body.position + Vector3.up;
 
@@ -79,8 +79,7 @@ public class PlayerActions : MonoBehaviour
         }
     }
 
-    private void Throw()
-    {
+    private void Throw() {
         Vector2 force = _inputHandler.aim * throwForce * _statModifiers.ThrowForceMult;
         _head.rb.linearVelocity = Vector2.zero;
         _head.rb.AddForce(force, ForceMode2D.Impulse);
@@ -89,10 +88,8 @@ public class PlayerActions : MonoBehaviour
         headAttached = false;
     }
 
-    public void Shoot()
-    {
-        if (currAmmo > 0 && currFireRate >= fireRate)
-        {
+    public void Shoot() {
+        if (currAmmo > 0 && currFireRate >= fireRate) {
             GameObject bullet = Instantiate(bulletPrefab, spawnPoint.position, Quaternion.identity);
             bullet.GetComponent<Bullet>().SetModifiers(_statModifiers.ExtraBounces,
                 _statModifiers.BulletSizeMult,
@@ -100,15 +97,15 @@ public class PlayerActions : MonoBehaviour
                 _statModifiers.BulletMassMult,
                 _statModifiers.BulletGravityMult,
                 _statModifiers.DamageMult);
-            bullet.GetComponent<Bullet>().rb.AddForce(_inputHandler.aim * shootForce * _statModifiers.FireForceMult, ForceMode2D.Impulse);
-            
+            bullet.GetComponent<Bullet>().rb.AddForce(_inputHandler.aim * shootForce * _statModifiers.FireForceMult,
+                ForceMode2D.Impulse);
+
             currAmmo--;
             currFireRate = 0;
         }
     }
 
-    private void GunDirection()
-    {
+    private void GunDirection() {
         float deg = Mathf.Atan2(_inputHandler.aim.y, _inputHandler.aim.x) * Mathf.Rad2Deg;
         gun.transform.rotation = Quaternion.Euler(0, 0, deg - 90);
     }
