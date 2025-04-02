6c228e5c53936a43123e8f5c5cff38b090a9a70a
JayQkew <rayjebb@gmail.com>
2025-04-02 23:16:20 +0200

    multiplayer

--- Big-Heads/Assets/_Scripts/InputHandler.cs
+++ Big-Heads/Assets/_Scripts/InputHandler.cs
@@ -8,74 +8,68 @@ public class InputHandler : MonoBehaviour
 {
     public Gamepad gamepad;
     public bool isLeftStickAim = true;
+
     [Space(10)]
     public Vector2 move;
     public Vector2 aim;
     [SerializeField] private bool shooting;
     [SerializeField] private bool jumping;
     [SerializeField] private bool teleportThrowing;
-    
+
     [Header("Events")]
     public UnityEvent onJump;
-    [Space(25)] 
+
+    [Space(25)]
     public UnityEvent onTeleportThrow;
+
     [Space(25)]
     public UnityEvent onThrow;
+
     [Space(25)]
     public UnityEvent onShoot;
+
     public UnityEvent onShootStart;
     public UnityEvent onShootEnd;
 
-    private void Update()
-    {
+    private void Update() {
         Aim();
-        if(shooting) onShoot?.Invoke();
+        if (shooting) onShoot?.Invoke();
     }
 
-    private void Aim()
-    {
-        aim = gamepad.rightStick.ReadValue() != Vector2.zero ?
-            gamepad.rightStick.ReadValue() :
-            gamepad.leftStick.ReadValue();
+    private void Aim() {
+        aim = gamepad.rightStick.ReadValue() != Vector2.zero
+            ? gamepad.rightStick.ReadValue()
+            : gamepad.leftStick.ReadValue();
     }
-    
+
     public void Move(InputAction.CallbackContext ctx) => move = ctx.ReadValue<Vector2>();
 
-    public void Jump(InputAction.CallbackContext ctx)
-    {
-        if (ctx.performed)
-        {
+    public void Jump(InputAction.CallbackContext ctx) {
+        if (ctx.performed) {
             onJump?.Invoke();
             jumping = true;
         }
-        else if (ctx.canceled)
-        {
+        else if (ctx.canceled) {
             jumping = false;
         }
     }
 
-    public void TeleportThrow(InputAction.CallbackContext ctx)
-    {
-        if (ctx.performed)
-        {
+    public void TeleportThrow(InputAction.CallbackContext ctx) {
+        if (ctx.performed) {
             onTeleportThrow?.Invoke();
             teleportThrowing = true;
         }
-        else if (ctx.canceled)
-        {
+        else if (ctx.canceled) {
             teleportThrowing = false;
         }
     }
 
-    public void Shoot(InputAction.CallbackContext ctx)
-    {
-        if (ctx.performed)
-        {
+    public void Shoot(InputAction.CallbackContext ctx) {
+        if (ctx.performed) {
             onShootStart?.Invoke();
             shooting = true;
         }
-        else if (ctx.canceled)
-        {
+        else if (ctx.canceled) {
             onShootEnd?.Invoke();
             shooting = false;
         }
