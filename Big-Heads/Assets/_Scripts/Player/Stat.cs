1b4b6fe6cf00d265dcac2519466747de5f74c06a
JayQkew <rayjebb@gmail.com>
2025-04-04 00:09:39 +0200

    finish stat handler
    next will add modifier system

--- Big-Heads/Assets/_Scripts/Player/Stat.cs
+++ Big-Heads/Assets/_Scripts/Player/Stat.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public interface IStat
 {
+    Stat Name { get; }
     void ApplyModifier(IStatModifier modifier);
 }
 
-public interface IStatModifier{}
+public interface IStatModifier
+{
+    void AdditiveModifier(float amount);
+}
 
 [Serializable]
-public abstract class Stat<T> where T : struct, IComparable<T>
+public abstract class Stat<T> : IStat where T : struct, IComparable<T>
 {
+    public Stat Name { get; private set; }
+    public abstract void ApplyModifier(IStatModifier modifier);
+
     public T BaseValue { get; private set; }
     public T MinValue { get; private set; }
     public T MaxValue { get; private set; }
-    
+
     private T _value;
 
     public virtual T Value
@@ -24,16 +31,15 @@ public abstract class Stat<T> where T : struct, IComparable<T>
         set => _value = Clamp(value);
     }
 
-    protected Stat(T baseValue, T minValue, T maxValue)
-    {
+    protected Stat(T baseValue, T minValue, T maxValue, Stat name) {
         BaseValue = baseValue;
         MinValue = minValue;
         MaxValue = maxValue;
         Value = baseValue; // Initialize value properly
+        Name = name;
     }
 
-    protected virtual T Clamp(T newValue)
-    {
+    protected virtual T Clamp(T newValue) {
         if (Comparer<T>.Default.Compare(newValue, MinValue) < 0) return MinValue;
         if (Comparer<T>.Default.Compare(newValue, MaxValue) > 0) return MaxValue;
         return newValue;
@@ -41,49 +47,48 @@ public abstract class Stat<T> where T : struct, IComparable<T>
 }
 
 [Serializable]
-public class IntStat : Stat<int> , IStat
+public class IntStat : Stat<int>
 {
     public StatInt stat;
 
     public override int Value
     {
         get => stat.val;
-        set
-        {
-            stat.val = Clamp(value);
-        }
+        set { stat.val = Clamp(value); }
     }
-    public IntStat(StatInt statInt) : base(statInt.val, statInt.min, statInt.max) {
+
+    public IntStat(StatInt statInt) : base(statInt.val, statInt.min, statInt.max, statInt.stat) {
         stat.val = Value;
         stat.min = MinValue;
         stat.max = MaxValue;
+        stat.stat = Name;
     }
 
-    public void ApplyModifier(IStatModifier modifier) {
+    
+    public override void ApplyModifier(IStatModifier modifier) {
         throw new NotImplementedException();
     }
 }
 
 [Serializable]
-public class FloatStat : Stat<float> , IStat
+public class FloatStat : Stat<float>
 {
     public StatFloat stat;
 
     public override float Value
     {
         get => stat.val;
-        set
-        {
-            stat.val = Clamp(value);
-        }
+        set { stat.val = Clamp(value); }
     }
-    public FloatStat(StatFloat statFloat) : base(statFloat.val, statFloat.min, statFloat.max) {
+
+    public FloatStat(StatFloat statFloat) : base(statFloat.val, statFloat.min, statFloat.max, statFloat.stat) {
         stat.val = Value;
         stat.min = MinValue;
         stat.max = MaxValue;
+        stat.stat = Name;
     }
 
-    public void ApplyModifier(IStatModifier modifier) {
+    public override void ApplyModifier(IStatModifier modifier) {
         throw new NotImplementedException();
     }
 }
@@ -91,22 +96,45 @@ public class FloatStat : Stat<float> , IStat
 [Serializable]
 public struct StatInt
 {
+    public Stat stat;
     public int val;
+
     [Space(10)]
     public int min;
+
     public int max;
 }
 
 [Serializable]
 public struct StatFloat
 {
+    public Stat stat;
     public float val;
+
     [Space(10)]
     public float min;
+
     public float max;
 }
 
 public enum Stat
 {
-    
+    Health,
+    Damage,
+    Speed,
+    Jump,
+    Teleport,
+    Throw,
+    HeadBounce,
+    HeadSize,
+    HeadMass,
+    HeadGravity,
+    Ammo,
+    FireRate,
+    FireForce,
+    BulletBounces,
+    BulletBounciness,
+    BulletSize,
+    BulletMass,
+    BulletGravity
 }
--- Big-Heads/Assets/_Scripts/Player/StatHandler.cs
+++ Big-Heads/Assets/_Scripts/Player/StatHandler.cs
@@ -1,23 +1,31 @@
 using System;
 using System.Collections.Generic;
+using NUnit.Framework.Internal;
 using UnityEngine;
 
 public class StatHandler : MonoBehaviour
 {
-    public List<IStat> stats;
-    
-    public IntStat health;
-    public FloatStat speed;
-    
+    private List<IStat> _stats = new List<IStat>();
+
+    public List<IntStat> intStats;
+    public List<FloatStat> floatStats;
 
     private void Start() {
-        health =  new IntStat(health.stat);
-        stats.Add(health);
-        speed = new FloatStat(speed.stat);
+        for (int i = 0; i < intStats.Count; i++) {
+            intStats[i] = new IntStat(intStats[i].stat);
+            _stats.Add(intStats[i]);
+            Debug.Log(intStats[i].stat.stat + ": "+ intStats[i].Value);
+        }
 
-        health.Value += 11;
+        for (int i = 0; i < floatStats.Count; i++) {
+            floatStats[i] = new FloatStat(floatStats[i].stat);
+            _stats.Add(floatStats[i]);
+            Debug.Log(floatStats[i].stat.stat + ": " + floatStats[i].Value);
+        }
         
-        Debug.Log(health.Value);
-        Debug.Log(speed.Value);
+    }
+
+    public IStat GetStat(Stat stat) {
+        return _stats.Find(s => s.Name == stat);
     }
 }
