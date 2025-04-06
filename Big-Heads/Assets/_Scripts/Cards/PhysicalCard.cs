4d90666a2a4fb776a3d4da208e389dc6a5c14266
JayQkew <rayjebb@gmail.com>
2025-04-06 11:29:07 +0200

    adding deck

--- Big-Heads/Assets/_Scripts/Cards/PhysicalCard.cs
+++ Big-Heads/Assets/_Scripts/Cards/PhysicalCard.cs
@@ -2,18 +2,25 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Card : MonoBehaviour
+public class PhysicalCard : MonoBehaviour
 {
+    public Card card;
+
+    private void Awake() {
+        card.TransferModifiers();
+    }
+}
+
+[Serializable]
+public class Card
+{
+    public string name;
     public List<IModifier> modifiers = new List<IModifier>();
     
     [SerializeField] private List<IntModifier> intModifiers = new List<IntModifier>();
     [SerializeField] private List<FloatModifier> floatModifiers = new List<FloatModifier>();
 
-    private void Awake() {
-        TransferModifiers();
-    }
-
-    private void TransferModifiers() {
+    public void TransferModifiers() {
         modifiers.Clear();
         
         foreach (IntModifier mod in intModifiers) {
